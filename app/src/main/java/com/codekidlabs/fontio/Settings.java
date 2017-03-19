ffbf6b5e5e91ab0484f059b21e2a79471752f5e5
codekidX <ckrackerx15@gmail.com>
2017-03-19 14:38:30 +0530

    Settings: add changelog dialog

--- app/src/main/java/com/codekidlabs/fontio/Settings.java
+++ app/src/main/java/com/codekidlabs/fontio/Settings.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ListView;
 
 import com.codekidlabs.bruno.services.CacheService;
@@ -34,6 +36,42 @@ public class Settings extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_about);
+
+            PreferenceScreen preferenceScreen = getPreferenceScreen();
+
+            Preference changeLogPref = preferenceScreen.findPreference("changelog_pref");
+
+            changeLogPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    showChangelogDialog();
+                    return true;
+                }
+            });
+        }
+
+        private void showChangelogDialog() {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            WebView changesView = new WebView(getActivity());
+            changesView.loadUrl("https://github.com/codekidX/Fontio/blob/master/CHANGES.md");
+            changesView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+
+                    return true;
+                }
+            });
+
+            alert.setView(changesView);
+            alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            alert.show();
         }
 
         @Override
@@ -47,14 +85,18 @@ public class Settings extends AppCompatActivity {
 
     public static class ConfigPreference extends PreferenceFragment {
 
+        Context context;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_config);
+            context = getActivity().getApplicationContext();
 
             PreferenceScreen preferenceScreen = getPreferenceScreen();
 
             SwitchPreference cacheSwitch = (SwitchPreference) preferenceScreen.findPreference("cache_switch");
+            SwitchPreference installSwitch = (SwitchPreference) preferenceScreen.findPreference("autoinstall_switch");
             final Preference resetPref = preferenceScreen.findPreference("reset_cache");
 
             invalidateSwitch(resetPref);
@@ -84,7 +126,7 @@ public class Settings extends AppCompatActivity {
                             .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-                                    new TtfService().resetCache(getActivity().getApplicationContext());
+                                    new TtfService().resetCache(context);
                                 }
                             })
                             .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
@@ -101,11 +143,11 @@ public class Settings extends AppCompatActivity {
         }
 
         private void invalidateSwitch(Preference resetPref) {
-            if(!PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()).contains("cache_switch")) {
+            if(!PreferenceManager.getDefaultSharedPreferences(context).contains("cache_switch")) {
                 resetPref.setEnabled(false);
             }
 
-            if(new CacheService(getActivity().getApplicationContext()).isLimitReached()) {
+            if(new CacheService(context).isLimitReached()) {
                 resetPref.setSummary("Cache Limit Reached.");
             }
         }
