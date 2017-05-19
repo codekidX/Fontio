2ae0df2ae288572de60cd57f171d593f33223d91
codekidX <ckrackerx15@gmail.com>
2017-05-19 17:09:18 +0530

    UI: apply dark mode to change-quote sheet

--- app/src/main/java/com/codekidlabs/fontio/PreviewTextSheet.java
+++ app/src/main/java/com/codekidlabs/fontio/PreviewTextSheet.java
@@ -1,19 +1,33 @@
 package com.codekidlabs.fontio;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 
 public class PreviewTextSheet extends BottomSheetDialogFragment {
 
+    SharedPreferences preferences;
+
     EditText editText;
     ImageButton setButton;
+
+    RelativeLayout sheetContent;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+    }
 
     @Nullable
     @Override
@@ -25,6 +39,7 @@ public class PreviewTextSheet extends BottomSheetDialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         View v = getView();
+        sheetContent = (RelativeLayout) v.findViewById(R.id.content_sheet_preview);
         editText = (EditText) v.findViewById(R.id.preview_et);
         setButton = (ImageButton) v.findViewById(R.id.change_preview_button);
 
@@ -35,5 +50,15 @@ public class PreviewTextSheet extends BottomSheetDialogFragment {
                 PreviewTextSheet.this.dismiss();
             }
         });
+
+
+        if(preferences.getBoolean("dm_switch", false)) {
+            sheetContent.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.bg_dark));
+
+            setButton.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.bg_dark_buddy));
+            setButton.setColorFilter(ContextCompat.getColor(getContext(), android.R.color.white));
+
+            editText.setHintTextColor(ContextCompat.getColor(getContext(), R.color.bg_dark_text));
+        }
     }
 }
